28bb24cf2b7cb0d253713b1117f4f1fd8efb27fc
ashwith0111 <69070712+ashwith0111@users.noreply.github.com>
2024-11-23 02:08:33 +0530

    Update

---
+++ fj.c
@@ -0,0 +1,17 @@
+Filling jars
+#include<stdio.h>
+int main(){
+        int j,o,a=0;
+        scanf("%d %d",&j,&o);
+        int can[j]={0};
+        for(int i=0;i<o;i++){
+                int u,l,v;
+                scanf("%d %d %d",&l,&u,&v);
+                for(int k=l-1;k<u;k++){
+                        can[k]+=v;
+                }
+        }
+        for(int i=0;i<j;i++){
+                printf("%d\n",can[i]);
+        }
+}
